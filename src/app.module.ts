3c28f31ee803aa303f354f36d190f5dbd2e21f42
hjpark625 <zero950@naver.com>
2023-06-11 15:17:04 +0900

    chore: 불필요 파일 및 코드 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import type { MongooseModuleOptions } from '@nestjs/mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { config } from 'dotenv';
+import type { MongooseModuleOptions } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 config();
@@ -17,20 +15,6 @@ config();
         useUnifiedTopology: true,
       }),
     }),
-    // MongooseModule.forRoot(process.env.MONGO_URI, {
-    //   connectionName: 'front-yard',
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   connectionFactory: (connection) => {
-    //     connection.on('connected', () => {
-    //       console.info('Connected to database');
-    //     });
-    //     connection.on('error', (error: unknown) => {
-    //       console.error(`Error connecting to database: ${error}`);
-    //     });
-    //     return connection;
-    //   },
-    // }),
     AuthModule,
   ],
   controllers: [],
