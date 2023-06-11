acbb2bbad6c16a72cb13b639ad1776d4231dd3bb
hjpark625 <zero950@naver.com>
2023-06-11 10:20:02 +0900

    feat: mongoDB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      connectionName: 'front-yard',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectionFactory: (connection) => {
+        connection.on('connected', () => {
+          console.info('Connected to database');
+        });
+        connection.on('error', (error: unknown) => {
+          console.error(`Error connecting to database: ${error}`);
+        });
+        return connection;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
